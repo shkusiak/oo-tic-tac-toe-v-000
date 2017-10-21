bab48e90ee585d10ad24d0132a5b713721aafe9a
Shea Kusiak <shkusiak@gmail.com>
2017-10-21 18:59:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,9 +22,9 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def input_to_index#(user_input)
-#    @user_input = user_input
-#    user_input.to_i - 1
+  def input_to_index
+  #  user_input = gets.strip
+  #  user_input.to_i - 1
   #  puts @board
   end
 
