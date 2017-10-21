b5d664716d19ed65aed6483668ffcf2bf15edcc3
Shea Kusiak <shkusiak@gmail.com>
2017-10-21 18:52:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,12 +23,14 @@ class TicTacToe
   end
 
   def input_to_index
+    user_input.to_i - 1
+    puts @board
   end
-  
+
   #def current_player
   #  turn_count % 2 == 0 ? "X" : "O"
   #end
-  
+
   #def turn_count
   #  @board.count{|token| token == "X" || token == "O"}
   #end
