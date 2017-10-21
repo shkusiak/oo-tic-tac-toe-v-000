3b88b9633c325445e8dfda3a7fd315c7cc6c4928
Shea Kusiak <shkusiak@gmail.com>
2017-10-21 18:58:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,8 +23,8 @@ class TicTacToe
   end
 
   def input_to_index(user_input)
-    @user_input = user_input
-    user_input.to_i - 1
+#    @user_input = user_input
+#    user_input.to_i - 1
   #  puts @board
   end
 
